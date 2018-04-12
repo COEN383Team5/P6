fc8bfe0d24f574016502572aa8850b1993952055
RyanLoringCooper <RyanLoringCooper@gmail.com>
2018-04-11 23:35:26 -0700

    output is good

--- P6.c
+++ P6.c
@@ -12,11 +12,11 @@
 #define numChildren 5
 #define READ_PIPE 0
 #define WRITE_PIPE 1
-#define TIME_TO_RUN 30
+#define TIME_TO_RUN 3
 #define MAX_SLEEP_TIME 2
 
 #define TIME_BUFF_SIZE 10
-#define BUFF_SIZE 64
+#define BUFF_SIZE 32
 
 struct timeval startTV;
 
@@ -48,7 +48,7 @@ void freePipes(int ***pipes) {
  */
 char *getTime() {
     struct timeval now;
-    char *retval = (char *)calloc(TIME_BUFF_SIZE, sizeof(char));
+    char *retval = (char *)calloc(TIME_BUFF_SIZE+1, sizeof(char));
     gettimeofday(&now, NULL);
     int msSinceStart = (now.tv_usec-startTV.tv_usec)/1000;
     int secsSinceStart = now.tv_sec-startTV.tv_sec;
@@ -59,8 +59,8 @@ char *getTime() {
 void readFromPipes(int ***pipesRef) {
     struct timeval tv;
     char buff[BUFF_SIZE], *timeBuff;
-    fd_set readSet[numChildren];
-    int i, selectVal, **pipes = *pipesRef, largestFD = 0;
+    fd_set readSet;
+    int i, readVal, selectVal, **pipes = *pipesRef, largestFD = 0;
     time_t startTime = 0;
     FILE *outputFile = fopen("output.txt", "w");
     if(outputFile == NULL) {
@@ -68,43 +68,47 @@ void readFromPipes(int ***pipesRef) {
         exit(errno);
     }
 
-    tv.tv_sec = 2;
-    tv.tv_usec = 0;
+    tv.tv_sec = 0;
+    tv.tv_usec = 500000;
     for(i = 0; i < numChildren; i++) {
         close(pipes[i][WRITE_PIPE]);
-        FD_ZERO(&readSet[i]);
-        FD_SET(pipes[i][READ_PIPE], &readSet[i]);
         if(pipes[i][READ_PIPE] > largestFD) {
             largestFD = pipes[i][READ_PIPE];
         }
     }
-    largestFD++;
     
     startTime = time(0);
     gettimeofday(&startTV, NULL);
     while(time(0)-startTime < TIME_TO_RUN) {
+        FD_ZERO(&readSet);
         for(i = 0; i < numChildren; i++) {
-            selectVal = select(pipes[i][READ_PIPE]+1, &readSet[i], NULL, NULL, &tv);
-            printf("%d selectVal\n", selectVal);
-            tv.tv_sec = 4;
-            tv.tv_usec = 0;
-            if(selectVal == -1) {
-                fprintf(stderr, "select failed with errno %d\n", errno);
-            } else if(selectVal) {
-                // read from a pipe
-                if(read(pipes[i][READ_PIPE], &buff, BUFF_SIZE) > 0) {
-                    timeBuff = getTime();
-                    fwrite(timeBuff, sizeof(char), 10, outputFile);
-                    fwrite(buff, sizeof(char), BUFF_SIZE, outputFile);
-                    printf("timeBuff = %s buff=%s", timeBuff, buff);
-                    memset(buff, 0, sizeof(char)*BUFF_SIZE);
-                    free(timeBuff);
-                    timeBuff = NULL;
-                } else {
-                    fprintf(stderr, "read failed with errno %d\n", errno);
-                }
-            } 
+            FD_SET(pipes[i][READ_PIPE], &readSet);
         }
+        selectVal = select(largestFD+1, &readSet, NULL, NULL, &tv);
+        printf("%d selectVal\n", selectVal);
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
+        if(selectVal == -1) {
+            fprintf(stderr, "select failed with errno %d\n", errno);
+        } else if(selectVal) {
+            // read from a pipes that were set by select
+            for(i = 0; i < numChildren; i++) {
+                if(FD_ISSET(pipes[i][READ_PIPE], &readSet)) {
+                    if((readVal = read(pipes[selectVal][READ_PIPE], &buff, BUFF_SIZE)) > 0) {
+                        timeBuff = getTime();
+                        // subtract 1 to avoid writting NULL byte
+                        fwrite(timeBuff, sizeof(char), TIME_BUFF_SIZE-1, outputFile);
+                        fwrite(buff, sizeof(char), readVal-1, outputFile);
+                        printf("timeBuff = %s buff=%s", timeBuff, buff);
+                        memset(buff, 0, sizeof(char)*BUFF_SIZE);
+                        free(timeBuff);
+                        timeBuff = NULL;
+                    } else if(readVal < 0) {
+                        fprintf(stderr, "read failed with errno %d\n", errno);
+                    }
+                }
+            }
+        } 
     }
 
     fclose(outputFile);
@@ -132,8 +136,8 @@ void writeToPipe(int **pipe, int childNum) {
         }
         timeBuff = getTime();
         snprintf(buff, BUFF_SIZE, "%s Child %d message %d\n", timeBuff, childNum, messageNum);
-        write((*pipe)[WRITE_PIPE], buff, sizeof(char)*BUFF_SIZE);
-//        printf("wrote %s\n", buff);
+        write((*pipe)[WRITE_PIPE], buff, strlen(buff)+1);
+        printf("wrote %s\n", buff);
         free(timeBuff);
         timeBuff = NULL;
     }
@@ -156,8 +160,8 @@ void lastChild(int **pipe) {
   //          printf("**** CHILD FIVE ****\n");
             messageNum++;
             timeBuff = getTime();
-            snprintf(buff, BUFF_SIZE, "%s Child 5 message %d\n", timeBuff, messageNum);
-            write((*pipe)[WRITE_PIPE], buff, sizeof(char)*BUFF_SIZE);
+            snprintf(buff, BUFF_SIZE, "%s Child 5 message %d\n\0", timeBuff, messageNum);
+            write((*pipe)[WRITE_PIPE], buff, strlen(buff+1));
             free(timeBuff);
             timeBuff = NULL;
         }
